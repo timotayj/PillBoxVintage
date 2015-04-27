b0626b9e86fa3a1f9cd1b433a4ccb81693ec47a3
unknown <johnsont@WIN764-1010265.livad.liv.ac.uk>
2015-04-27 16:04:10 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+var path = require('path');
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+
+var app = express();
+/*var routes = require('./routes/index');*/
+
+
+
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(stormpath.init(app, {
+    apiKeyId: 'xxxx',
+    apiKeySecret: 'xxxx',
+    application: 'xxxx',
+    secretKey: 'xxxx',
+}));
+
+app.get('/', function(req, res){
+    res.render('index', {
+        title: 'Home'
+    });
+});
+
+
+
+
+app.get('/dashboard', function(req, res){
+    res.render('dashboard', {
+        title: 'dash'
+    });
+});
+
+app.get('/secret', stormpath.loginRequired, function(req, res) {
+    res.send("If you're seeing this page, you must be logged in!");
+});
+app.listen(3001);
